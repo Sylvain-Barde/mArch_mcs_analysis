174da2d4d62674731f0983c4181886fafea7d509
Sylvain_Barde <s.barde@kent.ac.uk>
2024-07-16 16:07:36 +0100

    Readme write-up

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
 # mArch_mcs_analysis
-Replication material for "A fast algorithm for finding the confidence set of large model collections"
+
+This repository contains the replication files for "A fast algorithm for finding the confidence set of large model collections"
+
+## Requirements and Installation
+
+Running thre replication files require:
+- The `fastMCS` toolbox with dependencies installed.
+- The `mArch` toolbox with dependencies installed.
+- Additional packages specified in `requirements_pip.txt` and `requirements_conda.txt`, to be installed using `pip` and `conda` respectively.
+
+*NOTE*: This repo contains a copy of the `fastMCS` and `mArch` toolboxes in the state used for generating the results/figures in the release. These copies are not updated/maintained, for the latest versions of these toolboxes, refer to the relevant repository.
+
+*NOTE*: the main run files were are parallelised on HPC nodes, therefore any attempt at replication should take into account this computational requirement. This is particularly the3 case for the Monte-Carlo analysis, which assumes 20 cores are available with 25GB per core. The files are provided for the sake of transparency and replication, and all results are provided in the associated release (see below).
+
+## Release contents
+
+The release provides zipped versions of the following folders. These contain all the intermediate results of the scripts, so that the outputs of the paper (i.e. figures /tables) can be generated directly from them, without requiring a full re-run of the entire analysis.
+
+- `/data`: contains the empirical data used in the multivariate ARCH MCS analysis (Oxford-Man realized volatility library of Heber *et. al* 2009).
+- `/forecasts`: contains the raw 4800 multivariate ARCH forecasts used in the MCS analysis
+- `/logs`: contains the run logs of all the parallelised scripts (MCS benchmarking, forecast generation and MCS partition analysis)
+- `/losses`: contains the forecasts losses used in the MCS and partition analyses, as well as the results of the analyses.
+- `/montecarlo`: contains results and outputs of the Monte-Carlo benchmarking of the fast updating MCS algorithm (section 4 of the manuscript).
+- `/outputs`: contains outputs of the multivariate ARCH MCS analysis.
+
+## Run sequence:
+
+The various scripts should be run in the following order, as the outputs of earlier scripts for the inputs of later ones. To run a later file (e.g. output generation) without running an earlier file (e.g. estimation), use the folders provided in the release as the source of the intermediate inputs.
+
+### 1. Monte-Carlo benchmarking analysis (Section 4)
+
+- `parallel_fastMCS_benchmark.py` - Run the parallelised Monte Carlo benchmarking exercise.
+- `fastMCS_benchmark_outputs.py` - Generate outputs for the Monte Carlo benchmarking exercise.
+
+### 2. Multivariate ARCH forecast comparison (Section 5)
+
+#### 2.1. Forecast generation
+
+- `mArch_data_plots.py` - Generate the plots for the sample visualisation (figure 2 in manuscript).
+- `parallel_mArch_main.py` - Run the parallelised Multivariate ARCH forecasting script (2 runs, once per sample).
+
+#### 2.2. Loss calculation and MCS analysis
+
+- `mArch_loss_calculation.py` - Calculate the 8 sets of forecast losses (2 samples, 2 horizons and 2 volatility proxies).
+- `parallel_mArch_mcs_partition.py` - Run the parallelised 16 MCS partition analysis (2 bootstrap specifications per loss).
+- `mArch_mcs_outputs.py` - Run the full MCS analysis and generate the tables for the paper (summary table components for main body, full tables for appendix)
--- mArch_mcs_outputs.py
+++ mArch_mcs_outputs.py
@@ -2,13 +2,13 @@
 """
 Created on Fri Mar 31 06:52:12 2023
 
-This script produces the outputs for the multivariate ARCH MCS comparison 
+This script produces the outputs for the multivariate ARCH MCS comparison
 exercise. (tables 1, 2 and 3 in the paper).
 
 NOTE: because these tables summarize both the MCS composition and the breakdown
-of the partition, users need to run both 'parallel_mArch_main.py' and 
-'parallel_mArch_mcs_partition.py' prior to running this script, so that both
-sets of results are available.
+of the partition, users need to run 'parallel_mArch_main.py',
+'mArch_loss_calculation.py' and 'parallel_mArch_mcs_partition.py' prior to
+running this script, so that both sets of results are available.
 
 @author: Sylvain Barde, University of Kent
 """
@@ -23,7 +23,7 @@ from fastMCS import mcs
 from statsmodels.iolib.table import SimpleTable
 #------------------------------------------------------------------------------
 def format_int_table(tab, colorTab = None, maxIntLen = None):
-    
+
     # Check if color tab is provided and matches
     if colorTab is None:
         colorTab = np.empty_like(tab.astype(float))
@@ -31,14 +31,14 @@ def format_int_table(tab, colorTab = None, maxIntLen = None):
     elif ~np.all(colorTab.shape == tab.shape):
         print('Table formatting mismatch')
         return
-    
+
     if maxIntLen is None:
         maxIntLen = np.ceil(
                         np.log10(
                             max(tab.flatten())
                                  )
                             ).astype(int)
-        
+
     # Format table, including colored background if provided
     formattedTable = []
     for row, colorRow in zip(tab, colorTab):
@@ -48,7 +48,7 @@ def format_int_table(tab, colorTab = None, maxIntLen = None):
                 formattedCell = ''
             else:
                 formattedCell = "\\C{{{:2d}}}".format(colorCell.astype(int))
-            
+
             if cell < 0:
                 formattedCell += "{:s}".format(' ')
             elif cell == 0:
@@ -57,7 +57,7 @@ def format_int_table(tab, colorTab = None, maxIntLen = None):
                 formattedCell += ("{:" + str(maxIntLen) + "d} ").format(cell)
             formattedRow.append(formattedCell)
         formattedTable.append(formattedRow)
-            
+
     return formattedTable
 #------------------------------------------------------------------------------
 
@@ -83,7 +83,7 @@ settings = {'samples' : [0,1],
 
 univarModsLabel = {'arch' : 'AR',
                   'garch':  'GA',
-                  'figarch':'FI', 
+                  'figarch':'FI',
                   'egarch': 'EG',
                   'harch':  'HA',
                   'midas':  'MI',
@@ -91,14 +91,14 @@ univarModsLabel = {'arch' : 'AR',
 
 mArchLabels = {'naive':'Naïve',
                'ccc':'CCC',
-               'dcc':'DCC', 
+               'dcc':'DCC',
                'dcca':'DCCA'}
 
 rvLabels = {'bv':'bv',
             'bv_ss':'bv_ss',
             'medrv':'medrv',
             'rk_parzen':'rk_pa',
-            'rk_th2':'rk_th2',   
+            'rk_th2':'rk_th2',
             'rk_twoscale':'rk_ts',
             'rsv':'rsv',
             'rsv_ss':'rsv_ss',
@@ -128,24 +128,24 @@ tablePartList = []
 fullTablePartList = []
 numPartitionsList = []
 for (sample, rvInd, btsrtp, horizon) in mcsSettingsArray:
-    
+
     sample = int(sample)
     horizon = int(horizon)
     fileNameIn = 'sample_{:d}_proxy_{:s}'.format(sample,rvInd)
-    fileNameOut = fileNameIn + '_horizon_{:d}_btsrtp_{:s}_mcs'.format(horizon, 
+    fileNameOut = fileNameIn + '_horizon_{:d}_btsrtp_{:s}_mcs'.format(horizon,
                                                                       btsrtp)
-    
+
     fil = open(loadPathMCS + '/' + fileNameIn + '.pkl','rb')
     datas = zlib.decompress(fil.read(),0)
     fil.close()
     lossDict = pickle.loads(datas,encoding="bytes")
     univarType = lossDict['univarType']
-    
+
     if os.path.isfile(loadPathMCS + '/' + fileNameOut + '.pkl'):
         print('MCS analysis already ran, loading results')
         mcsEst = mcs()
         mcsEst.load(loadPathMCS, fileNameOut)
-        
+
     else:
         print('No pre-existing MCS analysis')
         losses = lossDict['losses'][:,:,horizon-1].squeeze()
@@ -153,39 +153,39 @@ for (sample, rvInd, btsrtp, horizon) in mcsSettingsArray:
         mcsEst.addLosses(losses)
         mcsEst.run(B, b, bootstrap = btsrtp)
         mcsEst.save(loadPathMCS, fileNameOut)
-    
+
     modList = np.asarray(lossDict['modList'])
     inclInd, exclInd = mcsEst.getMCS(alpha = 0.1)
     inclMod = modList[inclInd]
-    
+
     mcsSize.append(len(inclInd))
     mcsInclInds.append(inclInd)
     mcsInclMods.append(inclMod)
-    
+
     # Pre-allocate empty tables
     modelSpecs = lossDict['modelSpecs']
     rows = np.unique(modelSpecs[:,3], return_counts=False)
     cols = np.unique(modelSpecs[:,2], return_counts=False)
     dists = np.unique(modelSpecs[:,1], return_counts=False)
-    
+
     rowLabels = [rvLabels[key] for key in rvLabels.keys()]
     colLabels = [mArchLabels[key] for key in mArchLabels.keys()]
     univarLabels = ['-'] + [univarModsLabel[key] for key in univarModsLabel.keys()]
-    
+
     # Summary table (top-level, only multivar & RV, in paper)
-    table = pd.DataFrame(data=np.zeros((len(rows),len(cols))), 
-                         index=rowLabels, 
+    table = pd.DataFrame(data=np.zeros((len(rows),len(cols))),
+                         index=rowLabels,
                          columns=colLabels)
-    
-    # Full table (detailed, all 4 dimensions, for appendix)                            
+
+    # Full table (detailed, all 4 dimensions, for appendix)
     rowList = [rowLabels , dists]
-    rowArray = [array for array in 
+    rowArray = [array for array in
                 np.array(np.meshgrid(*rowList)).T.reshape(-1,len(rowList)).T]
-    
+
     colList = [colLabels, univarLabels]
-    colArray = [array for array in 
+    colArray = [array for array in
                 np.array(np.meshgrid(*colList)).T.reshape(-1,len(colList)).T]
-    
+
     fullTable = pd.DataFrame(np.zeros((len(rows)*len(dists),
                                       len(cols)*(len(univarModsLabel)+1))),
                             index = rowArray,
@@ -194,28 +194,28 @@ for (sample, rvInd, btsrtp, horizon) in mcsSettingsArray:
     # Populate from each model in the included set
     for mod in inclMod:
         batch = int(np.floor(mod/50))
-        
+
         rowInd = rvLabels[modelSpecs[batch,3]]
         colInd = mArchLabels[modelSpecs[batch,2]]
         distInd = modelSpecs[batch,1]
         archInd = univarModsLabel[univarType[mod]]
-        
+
         table.loc[rowInd][colInd] += 1
         fullTable.loc[(rowInd,distInd),(colInd, archInd)] += 1
-    
+
     # Set divider columns to negative integer for formatting
     for colInd in colLabels:
         fullTable.loc[:,(colInd, '-')] -= 1
-    
+
     # Load Partition and generate shaded color tables
     if os.path.isfile(loadPathPartition + '/' + fileNameOut + '.pkl'):
         print('Partition analysis exists, loading results')
         fil = open(loadPathPartition + '/' + fileNameOut + '.pkl','rb')
         modPartition = pickle.loads(fil.read(),encoding="bytes")
         fil.close()
-        
-        tablePart = pd.DataFrame(data=np.zeros((len(rows),len(cols))), 
-                                 index=rowLabels, 
+
+        tablePart = pd.DataFrame(data=np.zeros((len(rows),len(cols))),
+                                 index=rowLabels,
                                  columns=colLabels)
         fullTablePart = pd.DataFrame(np.zeros((len(rows)*len(dists),
                                                len(cols)*(len(univarModsLabel)+1))),
@@ -225,7 +225,7 @@ for (sample, rvInd, btsrtp, horizon) in mcsSettingsArray:
         # Process partition into intensities (average) for summary table
         for mArchLabel in list(mArchLabels.keys()):
             for rvLabel in list(rvLabels.keys()):
-                
+
                 cond1 = modelSpecs[:,2] == mArchLabel
                 cond2 = modelSpecs[:,3] == rvLabel
                 modExtract = np.empty((0))
@@ -242,65 +242,65 @@ for (sample, rvInd, btsrtp, horizon) in mcsSettingsArray:
         # Process partition into intensities (average) for full table
         for (modIndBase, distInd, mArchLabel, rvLabel) in modelSpecs:
             modIndBase = int(modIndBase)
-            
+
             # Populate univariate indices on 1st pass
             if univarInds == []:
                 univarInds = {}
                 univarBatch = np.asarray(
-                    [univarType[key] for key in range(modIndBase, 
+                    [univarType[key] for key in range(modIndBase,
                                                       modIndBase+50)]
                                         )
                 for univarLabel in list(univarModsLabel.keys()):
                     univarInds[univarModsLabel[univarLabel]] = np.where(
                         univarBatch == univarLabel)[0]
-            
+
             for archInd, archIndLoc in univarInds.items():
                 rowInd = rvLabels[rvLabel]
                 colInd = mArchLabels[mArchLabel]
                 modInd = modIndBase + archIndLoc
-                
+
                 fullTablePart.loc[(rowInd,distInd),
                                   (colInd, archInd)] = np.nanmean(
                                       modPartition[modInd])
-                                      
+
         # Rescale entries
         numPartitions = int(max(modPartition))
         tablePart *= 75/max(modPartition)
         tablePart -= 75
         tablePart *= -1
-        
+
         fullTablePart *= 75/max(modPartition)
         fullTablePart -= 75
         fullTablePart *= -1
-        
+
         # Set NAN values to 0
         tablePart = tablePart.fillna(0)
         fullTablePart = fullTablePart.fillna(0)
-        
+
         # Set divider columns to nan for formatting
         for colInd in colLabels:
             fullTablePart.loc[:,(colInd, '-')] *= np.nan
-        
+
     else:   # If partition not available, return empty variables
         tablePart = None
         fullTablePart = None
         numPartitions = np.nan
-        
+
     # Append to list of tables for output formatting later on
     tableList.append(table)
     fullTableList.append(fullTable)
     tablePartList.append(tablePart)
     fullTablePartList.append(fullTablePart)
     numPartitionsList.append(numPartitions)
-    
+
     # Time/Memory diagnostics:
     t.append(mcsEst.stats[0]['time'])
     mem.append(mcsEst.stats[0]['memory use'])
-    mods.append(mcsEst.stats[0]['models processed'])    
+    mods.append(mcsEst.stats[0]['models processed'])
     print('Time taken: {:6.3f} secs - memory used: {:6.3f} MB'.format(
         t[-1],mem[-1]*10**-6))
 
-print('Average collection size: {:6.3f} secs'.format(np.mean(mods)))    
+print('Average collection size: {:6.3f} secs'.format(np.mean(mods)))
 print('Average time taken:      {:6.3f} secs'.format(np.mean(t)))
 print('Average memory used:     {:6.3f} MB'.format(np.mean(mem)*10**-6))
 
@@ -308,18 +308,18 @@ mcsSizes = np.concatenate((mcsSettingsArray,
                                  np.asarray(mcsSize)[:,None]),
                                     axis = 1)
 # Generate tables
-for (setting, 
-     tableDf, 
-     fullTableDf, 
-     tablePartDf, 
-     fullTablePartDf, 
-     numPartitions) in zip(mcsSettingsArray, 
+for (setting,
+     tableDf,
+     fullTableDf,
+     tablePartDf,
+     fullTablePartDf,
+     numPartitions) in zip(mcsSettingsArray,
                            tableList,
                            fullTableList,
                            tablePartList,
                            fullTablePartList,
                            numPartitionsList):
-   
+
     print(setting[2])
     name = r'/sample_{:s}_pred_{:s}_{:s}_h_{:s}.tex'.format(
                                                       setting[0],
@@ -335,12 +335,12 @@ for (setting,
         vol = 'high'
     else:
         vol = 'low'
-    
+
     if np.isnan(numPartitions):
         numPartitionsStr = 'No'
     else:
         numPartitionsStr = '{:d}'.format(numPartitions)
-    
+
     title = 'MCS, {:s} bootstrap, {:s} volatility, {:s} proxy, {:s}-day horizon ({:s} classes)'.format(
         setting[2],
         vol,
@@ -348,19 +348,19 @@ for (setting,
         setting[3],
         numPartitionsStr)
     tableDf_np = tableDf.to_numpy(dtype=int)
-    tableWithTot = np.concatenate((tableDf_np, 
+    tableWithTot = np.concatenate((tableDf_np,
                                    np.sum(tableDf_np,axis = 0)[None,:]),
                                   axis = 0)
     if tablePartDf is None:
         tablePartWithTot = None
     else:
         tablePartDf_np = tablePartDf.to_numpy(dtype=int)
-        tablePartWithTot = np.concatenate((tablePartDf_np, 
+        tablePartWithTot = np.concatenate((tablePartDf_np,
                                        np.nan*np.sum(tablePartDf_np,axis = 0)[None,:]),
                                       axis = 0)
-    
+
     formattedTableWithTot = format_int_table(tableWithTot, tablePartWithTot)
-    
+
     # Generate aggregated table from dataframe, print and save
     table = SimpleTable(
             formattedTableWithTot,
@@ -368,7 +368,7 @@ for (setting,
             headers=list(tableDf.columns),
             title=title
         )
-    
+
     print(table)
     if save is True:
         with open(savePath + name,'w') as f:
@@ -380,24 +380,24 @@ for (setting,
     for mgarchMod in ['N','C', 'D', 'A']:
         for label in univarLabels:
             fullHead.append('{:s}:{:s}'.format(mgarchMod,label))
-    
+
     fullTableDf_np = fullTableDf.to_numpy(dtype=int)
     if fullTablePartDf is None:
         fullTablePartDf_np = None
     else:
         # fullTablePartDf_np = fullTablePartDf.to_numpy(dtype=int)
         fullTablePartDf_np = fullTablePartDf.to_numpy(dtype=float)
-    
-    formattedFullTable = format_int_table(fullTableDf_np, 
-                                          fullTablePartDf_np, 
+
+    formattedFullTable = format_int_table(fullTableDf_np,
+                                          fullTablePartDf_np,
                                           maxIntLen = 2)
-    
+
     fullTable = SimpleTable(
             formattedFullTable,
             stubs=fullStubs,
             headers=fullHead,
             title=title,
-        )  
+        )
     if save is True:
         with open(savePath + nameFull,'w') as f:
             f.write(fullTable.as_latex_tabular(header=r'%s',stub=r'%s'))
